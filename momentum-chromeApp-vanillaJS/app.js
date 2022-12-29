a66aa52cd8cd3700c52fe493c16eaa1ede8d5473
jskozy <jinsu8596@gmail.com>
2022-12-29 22:02:22 +0900

    chromeApp-start

---
+++ momentum-chromeApp-vanillaJS/app.js
@@ -0,0 +1,10 @@
+const loginForm = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+  console.log("hello", loginInput.value);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
+
+
